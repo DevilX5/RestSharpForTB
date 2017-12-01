c64b09f65a47344afd7c0f39d7fd74d0a86535dc
DevilX5 <devilx5@qq.com>
2017-12-01 15:22:49 +0800

    添加项目文件。

---
+++ RestSharpForTB/Program.cs
@@ -0,0 +1,38 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestSharpForTB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cookies = GetCookies("","","");
+            Console.WriteLine(cookies);
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// 获取cookies
+        /// login_username和login_password根据页面定义的名称而定
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        static string GetCookies(string url,string username,string password)
+        {
+            var client = new RestClient();
+            client.BaseUrl = new Uri(url);
+            client.Authenticator = new SimpleAuthenticator("login_username", username, "login_password", password);
+            var request = new RestRequest(Method.POST);
+            IRestResponse response = client.Execute(request);
+            var ck = response.Cookies.SingleOrDefault(n => n.Name.Equals("JSESSIONID"));
+            return ck.Value;
+        }
+    }
+}
